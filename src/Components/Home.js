bafd09e868c37c0a13b4fbd0b5d634b1d1cbbfea
Kiran-25 <kiran472012@gmail.com>
2022-12-29 06:32:23 +0530

    Home file updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import { Button, Modal } from 'antd';
 
 import { IdleTimerProvider} from 'react-idle-timer'
 
- 
+ // dsd
 class Home extends Component{
     constructor(props){
         super(props);   
